321bb0dec70df2e98013a5163099ca83157d67e4
Mohamed Eid <m.mohamedeid11@gmail.com>
2022-10-24 21:26:56 +0200

    first push

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate(20);
+//        $posts = Post::all();
+
+        return  PostResource::collection($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        if ($post->save())
+        {
+            return new PostResource($post);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        if ($post->save())
+        {
+            return new PostResource($post);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        if ($post->delete())
+        {
+            return response('success to delete' , 200);
+//                ->header('Content-Type', 'text/plain');;
+        }
+    }
+}
